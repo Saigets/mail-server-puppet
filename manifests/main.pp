854cf642599423edde70dcaa0d3a1e87f7801ca5
Saigets <atochko@gmail.com>
2017-10-19 09:28:30 +0300

    added package ensure stdlib moved config to mian manifest

--- manifests/main.pp
+++ manifests/main.pp
@@ -1,9 +1,61 @@
-if $::vm_type == "vagrant" {
+#if $::vm_type == "vagrant" {
 #	import "config.pp"
+#}
+
+class config {
+	$mail_server_name = "MY.FQDN.COM"
+	$generate_certificate = "true"
+	if $::vm_type == "vagrant" {
+		$files = "/etc/puppet/files"
+	} else {
+		$files = "./files"
+	}
+
+	$memcache_memory = "128m"
+
+	if $generate_certificate == "true" {
+		$certificate = "ssl-cert-snakeoil.pem"
+		$certificate_key = "ssl-cert-snakeoil.key"
+	} else {
+		$certificate = "my-seed-cert.pem"
+		$certificate_key = "my-seed-cert.key"
+	}
+		
+	$maildb_user = 'mail'
+	$maildb_name = 'mail'
+	$maildb_pwd = get_password("/root/.mail.pwd")
+	
+	$mailadmin_user = "admin@$mail_server_name"
+	$mailadmin_pwd = get_password("/root/.mailadmin.pwd")
+	
+	$roundcube_user = 'roundcube'
+	$roundcube_name = 'roundcube'
+	$roundcube_pwd = get_password("/root/.roundcube.pwd")
+	
+	
+	$vimbadmin_salt1 = get_password("/root/.vimbadmin_salt1.pwd", 64)
+	$vimbadmin_salt2 = get_password("/root/.vimbadmin_salt2.pwd", 64)
+	$vimbadmin_salt3 = get_password("/root/.vimbadmin_salt3.pwd", 64)
+	
+	$restore_maildb_backup = "false"
+	#
+	# Number of processors to use for amavis (less means less memory, more means more emails at a time)
+	#
+	$amavis_process_count = 3
+
+	#
+	# Maximum allowed email size.
+	#
+	$message_size_limit = 10240000
+
+	
+	$backup_user_allowed_key = ""
 }
 
-class packages {
 
+class packages {
+	# puppet-stdlibs needed for functions like file_line
+	package { "puppet-module-puppetlabs-stdlib": ensure => present }
 	# nginx (www)
 	package { "apache2": 		ensure => absent }
 	->
@@ -208,7 +260,7 @@ class config_firewall {
 	ufw::allow { 'firewall https':
 		port  => 443,
 	}
-	ufw::allow { 'firewall https2':
+	ufw::allow { 'firewall https2 for admin':
 		port  => 444,
 	}
 	ufw::allow { 'firewall ssh':
